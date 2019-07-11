5f61edded75b5c5649d1ca998ff1075feebd7cab
Tom de Grunt <tom@degrunt.nl>
2019-07-11 23:06:53 +0200

    Add callback capability

--- lib/executo/configuration.rb
+++ lib/executo/configuration.rb
@@ -2,16 +2,25 @@ module Executo
   class Configuration
     attr_accessor :redis
     attr_writer   :logger
+    attr_writer   :callback
 
     def initialize
       @redis = {}
       @logger = Logger.new(STDOUT)
       @logger.level = Logger::DEBUG
+      @callback = ->(command, options, status, pid, stdout, stderr) {}
     end
 
     # logger [Object].
     def logger
       @logger.is_a?(Proc) ? instance_exec(&@logger) : @logger
+    end
+
+    # callback
+    def callback(command, options, exitstatus, pid, stdout, stderr)
+      return unless @callback.is_a?(Proc)
+
+      instance_exec(command, options, exitstatus, pid, stdout, stderr, &@callback)
     end
   end
 end
