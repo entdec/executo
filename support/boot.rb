a9239edd0dfc40670ffc861cdba5c3349b000b75
Tom de Grunt <tom@degrunt.nl>
2019-07-11 15:54:42 +0200

    Better samples

--- support/boot.rb
+++ support/boot.rb
@@ -1,5 +1,9 @@
 require 'executo'
 
 Executo.setup do |config|
-  config.redis = { url: "redis://10.10.2.36:6379/0" }
+  config.redis = { url: "redis://10.10.2.36:6379/1" }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = Executo.config.redis
 end
