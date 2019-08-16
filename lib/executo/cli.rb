2de9b668acff82e4c3e75a0d9cd4dd4260864463
Tom de Grunt <tom@degrunt.nl>
2019-08-16 11:22:26 +0200

    Remove chdir capability

--- lib/executo/cli.rb
+++ lib/executo/cli.rb
@@ -3,7 +3,7 @@ require 'shellwords'
 
 module Executo
   class CLI
-    def self.run(cmd, in_directory: nil, stdin_content: [], stdin_newlines: true, stdout:, stderr:)
+    def self.run(cmd, stdin_content: [], stdin_newlines: true, stdout:, stderr:)
       raise 'cmd must be a String or array of Strings.' \
         unless cmd.is_a?(String) || (cmd.is_a?(Array) && cmd.all? { |c| c.is_a?(String) })
       raise 'stdout must be a Proc.' unless stdout.is_a?(Proc)
@@ -22,28 +22,29 @@ module Executo
                      end
 
       Executo.config.logger.debug "computed cmd: #{computed_cmd}"
-      Dir.chdir(in_directory || Dir.pwd) do
-        Open3.popen3(computed_cmd) do |stdin_stream, stdout_stream, stderr_stream, thread|
-          Executo.config.logger.debug "thread: #{thread}"
-          stdin_content.each do |input_line|
-            stdin_stream.write(input_line)
+      Open3.popen3(computed_cmd) do |stdin_stream, stdout_stream, stderr_stream, thread|
+        Executo.config.logger.debug "thread: #{thread}"
+        stdin_content.each do |input_line|
+          stdin_stream.write(input_line)
 
-            stdin_stream.write("\n") unless input_line[-1] == "\n" || !stdin_newlines
-          end
+          stdin_stream.write("\n") unless input_line[-1] == "\n" || !stdin_newlines
+        end
 
-          stdin_stream.close
+        stdin_stream.close
 
-          { stdout_stream => stdout, stderr_stream => stderr }.each_pair do |stream, callback|
-            Thread.new do
+        { stdout_stream => stdout, stderr_stream => stderr }.each_pair do |stream, callback|
+          Thread.new do
+            begin
               until (line = stream.gets).nil?
                 callback.call(line)
               end
+            rescue IOError => e
+              # ignore
             end
-          end
-
-          thread.join
-          thread.value
         end
+
+        thread.join
+        thread.value
       end
     end
   end
