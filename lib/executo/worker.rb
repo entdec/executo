6a86a0ef0db145019f3da86afe2ccec906f3bfb5
Andre Meij <andre@itsmeij.com>
2020-09-30 09:28:57 +0200

    handle allllll the errrrrs

--- lib/executo/worker.rb
+++ lib/executo/worker.rb
@@ -15,37 +15,35 @@ module Executo
       Executo.config.logger.info 'Command started'
       feedback(options['feedback'], 'started')
 
-      begin
-        stdout = []
-        stderr = []
+      stdout = []
+      stderr = []
 
-        # CLI's callbacks could be used for progress reporting
-        status = CLI.run(
-          [command] + params,
-          stdout: ->(line) { stdout << line },
-          stderr: ->(line) { stderr << line },
-          stdin_content: options['stdin_content'] || [],
-          stdin_newlines: options.key?('stdin_newlines') ? options['stdin_newlines'] : true,
-          shell_escape: options.key?('shell_escape') ? options['shell_escape'] : true
-        )
+      # CLI's callbacks could be used for progress reporting
+      status = CLI.run(
+        [command] + params,
+        stdout: ->(line) { stdout << line },
+        stderr: ->(line) { stderr << line },
+        stdin_content: options['stdin_content'] || [],
+        stdin_newlines: options.key?('stdin_newlines') ? options['stdin_newlines'] : true,
+        shell_escape: options.key?('shell_escape') ? options['shell_escape'] : true
+      )
 
-        feedback(
-          options['feedback'],
-          status.success? ? 'completed' : 'failed',
-          status.exitstatus.to_i,
-          stdout.join,
-          stderr.join,
-          'command' => command,
-          'params' => params,
-          'options' => options,
-          'pid' => status.pid.to_i
-        )
-      rescue StandardError => e
-        # This only happens if something really broke, not worth a callback?
-        Executo.config.logger.error "Command failed with exception: #{e.class} - #{e.message}"
-        raise e
-      end
-
+      feedback(
+        options['feedback'],
+        status.success? ? 'completed' : 'failed',
+        status.exitstatus.to_i,
+        stdout.join,
+        stderr.join,
+        'command' => command,
+        'params' => params,
+        'options' => options,
+        'pid' => status.pid.to_i
+      )
+    rescue => e
+      Executo.config.logger.error "Command failed with exception: #{e.class} - #{e.message}"
+      Executo.config.logger.error e.backtrace.join("\n")
+      feedback(options['feedback'], 'failed')
+    ensure
       Executo.config.logger.info "Command finished, using pid #{status.pid}"
       feedback(options['feedback'], 'finished')
     end
