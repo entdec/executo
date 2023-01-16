76d5037f5c6eb29cca3754f7059cee339a552b44
Andre Meij <andre@itsmeij.com>
2023-01-16 17:32:55 +0100

    Meh, sbin <> bin

--- lib/executo/worker.rb
+++ lib/executo/worker.rb
@@ -51,7 +51,7 @@ module Executo
       shell_escape = options.key?('shell_escape') ? options['shell_escape'] : true
 
       Bundler.with_unbundled_env do
-        dir = command.start_with?('/') ? File.dirname(command).gsub!(/bin$/, '') : Dir.pwd
+        dir = command.start_with?('/') ? File.dirname(command).gsub!(/\/bin$/, '') : Dir.pwd
         dir = options['working_folder'] if options['working_folder'].present?
         dir = dir.presence || Dir.pwd
         CLI.run(argument_list, stdout: ->(line) { register_output(:stdout, line) }, stderr: ->(line) { register_output(:stderr, line) }, stdin_content: stdin_content, stdin_newlines: stdin_newlines, shell_escape: shell_escape, working_folder: dir)
