a188d4a74c24def0da11710ca8b87fb944a633c2
Andre Meij <andre@itsmeij.com>
2023-01-19 17:29:03 +0100

    FUCKING GROSS!

--- lib/executo/cli.rb
+++ lib/executo/cli.rb
@@ -31,7 +31,7 @@ module Executo
       def escaped_command(command, shell_escape: true)
         return command.join unless shell_escape
 
-        command.shelljoin
+        command.shelljoin.gsub('{SPATIE}', '\ ')
       end
 
       def write_stream(stream, content, newlines: true)
